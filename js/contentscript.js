1670ec2dec649445e6b405112396f48a07f4759b
Fady Ayoob <fadyayoobdev@gmail.com>
2018-03-16 11:27:11 +0200

    v1
    .

---
+++ js/contentscript.js
@@ -0,0 +1,12 @@
+//this script  created by "Fady Ayoob " email => fadyAyoobDev@gmail.com
+
+
+AddLoader();
+HideLoader();
+AddSaveButton();
+HideButton();
+GetSelectedText();
+LoadButtonPosition();
+
+setInterval(function () {load(); }, 500);
+
