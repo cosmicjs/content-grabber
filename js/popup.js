cd79108e1fcae00c3f8ace1660b10b24ed7422b2
Fady Ayoob <fadyayoobdev@gmail.com>
2018-03-16 17:58:17 +0200

    v1

---
+++ js/popup.js
@@ -0,0 +1,27 @@
+//this extention created by ----> fadyAyoobDev@gmail.com
+
+$(document).ready(function(){
+    $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+    
+    IntPopup();
+    
+    $("#loginForm").submit(function(){
+        var email=document.getElementById('email').value;
+        var password=document.getElementById('password').value;
+        Login(email,password);
+    });
+    
+    $(".login input").change(function(){
+        AddLoginMassage('');
+    })
+    
+    $("#try").click(function(){
+        CreateObject('','','');
+    });
+    
+    $("#logout").click(function(){
+        DeactivateLogin();
+    });
+});
